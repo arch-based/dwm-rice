6fdc28308ccacf002cb2182810c9a7c6bdebcd2c
Anonymous <anonymous.negation790@passinbox.com>
2024-05-12 10:40:22 +0800

    DWM Rice: (config.h)

--- config.h
+++ config.h
@@ -57,7 +57,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "pkill rofi; rofi", "-show", "drun", NULL };
+static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 static const char *firefoxcmd[] = { "firefox", NULL };
 static const char *filecmd[] = { "thunar", NULL };
