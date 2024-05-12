e3e48e038fd3266f18fef459b047ff42fd524dde
Anonymous <anonymous.negation790@passinbox.com>
2024-05-12 16:48:55 +0800

    DWM Rice: (config.h)

--- config.h
+++ config.h
@@ -59,10 +59,11 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *firefoxcmd[] = { "firefox", NULL };
+static const char *firefoxcmd[] = { "firejail", "firefox", NULL };
 static const char *filecmd[] = { "thunar", NULL };
 static const char *powermenu[] = { "powermenu", NULL };
 static const char *lockcmd[] = { "sh", "-c", "/home/arch/.local/bin/i3lock", NULL };
+static const char *clipmenu[] = { "clipmenu", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -72,6 +73,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_e,      spawn,          {.v = filecmd } },
 	{ MODKEY,                       XK_b,      spawn,          {.v = powermenu } },
 	{ MODKEY,                       XK_l,      spawn,          {.v = lockcmd } },
+	{ MODKEY,                       XK_period, spawn,          {.v = clipmenu } },
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
